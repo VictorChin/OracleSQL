a89229081c69ed731da58ececb0f6bd85b1d208b
victor chin <ypchin@yahoo.com>
2017-01-10 13:13:32 -0500

    add 2 column group by

--- DataWarehouseQuery.sql
+++ DataWarehouseQuery.sql
@@ -5,8 +5,10 @@ select Prod_ID,sum(Amount_Sold)
 From SH.Sales 
 Group by Prod_ID ;
 
-Select NVL(Prod_Category,'GrandTotal'), Sum(Amount_Sold)
+Select NVL(Prod_Category,'GrandTotal'),Prod_SubCategory,
+To_Char(Sum(Amount_Sold),'999,999,999.00') as TotalAmount
 From 
 Sales S JOIN Products P
         ON S.Prod_ID = P.Prod_ID
-Group by Rollup(Prod_Category);
+Group by Rollup(Prod_Category,Prod_SubCategory);
+
